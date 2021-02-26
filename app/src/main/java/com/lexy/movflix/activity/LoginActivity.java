d6162d22e4e1de291940ab195059eee2f8a3c602
ian lombu <citrakemurnian@gmail.com>
2021-02-27 01:07:53 +0700

    Setup Project

---
+++ app/src/main/java/com/lexy/movflix/activity/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.lexy.movflix.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.lexy.movflix.R;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button btn_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btn_login = findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/lexy/movflix/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.lexy.movflix.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lexy.movflix.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
